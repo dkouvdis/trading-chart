874cf68422c67ea979597447db49284aaa6c91ff
Dimitri Kouvdis <accounts@dkouvdis.com>
2024-09-20 13:23:56 +0300

    init commit
    asd

---
+++ main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+import { runLightweightChart } from "./lib/lightweight.js";
+import { runAdvancedChart } from "./lib/advanced-chart.js";
+
+// runLightweightChart().catch(console.error);
+runAdvancedChart().catch(console.error);
